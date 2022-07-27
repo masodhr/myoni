1deb4f50f917d31f9cb44afb5fef3ea345b53b39
ahmed <masod.mcr@gmail.com>
2022-07-27 13:14:06 +0530

    fixing july27 bug

---
+++ bugfix.py
@@ -0,0 +1,9 @@
+
+27-07-2022 --Wednesday-- notes
+
+
+*Terraform**
+ terraform apply
+                depends_on  (new feature in terraform)
+                 ami-09dlkje9 (you can see ami while creating a EC2 instance)
+
